c5cd927cf71eca0a04379b2ab8b71c51baf20229
Igor Kamyshev <igor@kamyshev.me>
2022-07-08 20:46:48 +0700

    Init showcases

---
+++ apps/showcase/json-react/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="showcase-json-react" />
+      <div />
+    </>
+  );
+}
+
+export default App;
