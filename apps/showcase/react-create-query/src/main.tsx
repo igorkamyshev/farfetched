55a079474234adbdda446696ac6367862ab3221e
Igor Kamyshev <igor@kamyshev.me>
2022-07-11 15:55:39 +0700

    simplify showcase

--- apps/showcase/react-create-query/src/main.tsx
+++ apps/showcase/react-create-query/src/main.tsx
@@ -1,9 +1,51 @@
-import { StrictMode } from 'react';
+import { createQuery, unkownContract } from '@farfetched/core';
+import { StrictMode, useState } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { useStore } from 'effector-react';
+import { createEffect } from 'effector';
 
-import 'antd/dist/antd.css';
+const pokemonsQuery = createQuery({
+  effect: createEffect(({ limit }: { limit: number }) =>
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`).then((response) =>
+      response.json()
+    )
+  ),
+  contract: unkownContract,
+  mapData: (data: any) => data.results as Array<{ name: string }>,
+});
 
-import App from './app/app';
+export function App() {
+  const pokemons = useStore(pokemonsQuery.$data);
+  const loading = useStore(pokemonsQuery.$pending);
+
+  const [limit, setLimit] = useState(10);
+
+  return (
+    <>
+      <h1>Pokemon 💙 Farfetched</h1>
+      <label>
+        Limit:
+        <input
+          min={1}
+          value={limit}
+          type="number"
+          onChange={(e) => setLimit(e.target.valueAsNumber)}
+        />
+      </label>
+      <button onClick={() => pokemonsQuery.start({ limit })}>
+        Load pokemons
+      </button>
+      {loading && <p>Loading...</p>}
+      {pokemons && (
+        <ol>
+          {pokemons.map((pokemon) => (
+            <li key={pokemon.name}>{pokemon.name}</li>
+          ))}
+        </ol>
+      )}
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
