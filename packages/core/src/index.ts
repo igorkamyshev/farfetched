4971fc1aa3d33680de362823b239c113ac4e569e
Igor Kamyshev <igor@kamyshev.me>
2022-07-11 17:17:59 +0700

    Showcase #1 (#6)
    * Init showcases
    
    * Add basic show case with react
    
    * Increase size limit for core
    
    * Finalize show-case
    
    * simplify showcase
    
    * Add link to showcase
    
    * Add some docs
    
    * Fix format

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,5 @@
 export { type Query } from './query/type';
 export { createQuery } from './query/create_query';
 export { type Contract } from './contract/type';
+export { unkownContract } from './contract/unkown_contract';
 export { connectQuery } from './query/connect_query';
