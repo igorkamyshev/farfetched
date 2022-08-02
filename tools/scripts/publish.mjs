8209829e43ac98e158a1bf1172f0166bc9ab4061
Igor Kamyshev <igor@kamyshev.me>
2022-08-02 19:20:05 +0700

    Simplify release workflow

--- tools/scripts/publish.mjs
+++ tools/scripts/publish.mjs
@@ -1,27 +1,17 @@
-/**
- * This is a minimal script to publish your package to "npm".
- * This is meant to be used as-is or customize as you see fit.
- *
- * This script is executed on "dist/path/to/library" as "cwd" by default.
- *
- * You might need to authenticate with NPM before running this script.
- */
-
-import { readCachedProjectGraph } from '@nrwl/devkit';
+import { readCachedProjectGraph, logger, readJsonFile } from '@nrwl/devkit';
 import { execSync } from 'child_process';
-import { readFileSync, writeFileSync } from 'fs';
 import chalk from 'chalk';
 
 function invariant(condition, message) {
   if (!condition) {
-    console.error(chalk.bold.red(message));
+    logger.error(chalk.bold.red(message));
     process.exit(1);
   }
 }
 
-// Executing publish script: node path/to/publish.mjs {name} --version {version} --tag {tag}
-// Default "tag" to "next" so we won't publish the "latest" tag by accident.
-const [, , name, version, tag = 'next'] = process.argv;
+const { version } = readJsonFile(`package.json`);
+
+const [, , name] = process.argv;
 
 // A simple SemVer validation to validate the version
 const validVersion = /^\d+\.\d+\.\d+(-\w+\.\d+)?/;
@@ -46,16 +36,4 @@ invariant(
 
 process.chdir(outputPath);
 
-// Updating the version in "package.json" before publishing
-try {
-  const json = JSON.parse(readFileSync(`package.json`).toString());
-  json.version = version;
-  writeFileSync(`package.json`, JSON.stringify(json, null, 2));
-} catch (e) {
-  console.error(
-    chalk.bold.red(`Error reading package.json file from library build output.`)
-  );
-}
-
-// Execute "npm publish" to publish
-execSync(`npm publish --access public --tag ${tag}`);
+execSync(`pnpm publish`, { stdio: 'inherit' });
