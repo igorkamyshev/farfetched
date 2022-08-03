d05d44466e7c659162d40d072690c68b9f69358f
Igor Kamyshev <igor@kamyshev.me>
2022-08-03 13:00:11 +0700

    Simplify release workflow

--- tools/scripts/publish.mjs
+++ tools/scripts/publish.mjs
@@ -1,28 +1,16 @@
-import { readCachedProjectGraph, logger, readJsonFile } from '@nrwl/devkit';
-import { execSync } from 'child_process';
+import {
+  readCachedProjectGraph,
+  logger,
+  readJsonFile,
+  writeJsonFile,
+} from '@nrwl/devkit';
+import { spawnSync } from 'child_process';
 import chalk from 'chalk';
 
-function invariant(condition, message) {
-  if (!condition) {
-    logger.error(chalk.bold.red(message));
-    process.exit(1);
-  }
-}
-
-const { version } = readJsonFile(`package.json`);
-
 const [, , name] = process.argv;
-
-// A simple SemVer validation to validate the version
-const validVersion = /^\d+\.\d+\.\d+(-\w+\.\d+)?/;
-invariant(
-  version && validVersion.test(version),
-  `No version provided or version did not match Semantic Versioning, expected: #.#.#-tag.# or #.#.#, got ${version}.`
-);
-
 const graph = readCachedProjectGraph();
-const project = graph.nodes[name];
 
+const project = graph.nodes[name];
 invariant(
   project,
   `Could not find project "${name}" in the workspace. Is the project.json configured correctly?`
@@ -36,4 +24,54 @@ invariant(
 
 process.chdir(outputPath);
 
-execSync(`pnpm publish`, { stdio: 'inherit' });
+const originalPackageJson = readJsonFile(`package.json`);
+
+writeJsonFile('package.json', {
+  ...originalPackageJson,
+  publishConfig: { access: 'public' },
+});
+
+const { version } = originalPackageJson;
+
+// A simple SemVer validation to validate the version
+const validVersion = /^\d+\.\d+\.\d+(-\w+\.\d+)?/;
+invariant(
+  version && validVersion.test(version),
+  `No version provided or version did not match Semantic Versioning, expected: #.#.#-tag.# or #.#.#, got ${version}.`
+);
+
+const result = spawnSync('npm', ['publish', '--json']);
+
+if (result.stderr.toString()) {
+  const errorInfo = getLastJsonObjectFromString(result.stderr.toString());
+  logger.warn('Skip publishing\n');
+  logger.warn(errorInfo.error.summary);
+} else {
+  logger.info(result.stdout.toString());
+  logger.info(chalk.green('Published successfully'));
+}
+
+// utils
+
+function getLastJsonObjectFromString(str) {
+  str = str.replace(/[^}]*$/, '');
+
+  while (str) {
+    str = str.replace(/[^{]*/, '');
+
+    try {
+      return JSON.parse(str);
+    } catch (err) {
+      // move past the potentially leading `{` so the regexp in the loop can try to match for the next `{`
+      str = str.slice(1);
+    }
+  }
+  return null;
+}
+
+function invariant(condition, message) {
+  if (!condition) {
+    logger.error(chalk.bold.red(message));
+    process.exit(1);
+  }
+}
