54fdf31b089b8366187bc7b3b2ff2f1ceac98b76
Igor Kamyshev <igor@kamyshev.me>
2022-08-05 13:47:14 +0700

    Bootstrap showcase

---
+++ apps/showcase/react-connect-query/src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+
+function App() {
+  return null;
+}
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
