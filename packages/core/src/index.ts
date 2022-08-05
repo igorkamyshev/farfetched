339ee39d1a120fba455c5acac133dcf1619e339f
Igor Kamyshev <igor@kamyshev.me>
2022-08-05 12:54:01 +0700

    Add error guards and export error-awared stuff

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,9 +1,39 @@
-export { type Query } from './query/type';
-export { createQuery } from './query/create_query';
 export { type Contract } from './contract/type';
 export { unkownContract } from './contract/unkown_contract';
+
+export { type Query } from './query/type';
+export { createQuery } from './query/create_query';
 export { connectQuery } from './query/connect_query';
 export { createHeadlessQuery } from './query/create_headless_query';
 export { createJsonQuery } from './query/create_json_query';
+
 export { declareParams } from './misc/params';
+
 export { type Json } from './fetch/json';
+
+export {
+  type FarfetchedError,
+  type InvalidDataError,
+  type TimeoutError,
+  type AbortError,
+  type PreparationError,
+  type HttpError,
+  type NetworkError,
+} from './errors/type';
+export {
+  invalidDataError,
+  timeoutError,
+  abortError,
+  preparationError,
+  httpError,
+  networkError,
+} from './errors/create_error';
+export {
+  isTimeoutError,
+  isAbortError,
+  isPreparationError,
+  isHttpError,
+  isNetworkError,
+  isInvalidDataError,
+  isHttpErrorCode,
+} from './errors/guards';
