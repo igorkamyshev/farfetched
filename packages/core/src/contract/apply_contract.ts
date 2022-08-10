a55c22a2e897f6f7f004bcfc585d0f838fda966d
Igor Kamyshev <igor@kamyshev.me>
2022-08-10 13:39:51 +0700

    Add sid for applyContractFx

--- packages/core/src/contract/apply_contract.ts
+++ packages/core/src/contract/apply_contract.ts
@@ -7,8 +7,8 @@ import { Contract } from './type';
 function createContractApplier<Raw, Data extends Raw, Error extends Raw>(
   contract: Contract<Raw, Data, Error>
 ): Effect<Raw, Data, Error | InvalidDataError> {
-  const applyContractFx = createEffect<Raw, Data, Error | InvalidDataError>(
-    (response) => {
+  const applyContractFx = createEffect<Raw, Data, Error | InvalidDataError>({
+    handler: (response) => {
       const isError = contract.isError(response);
       if (isError) {
         throw response;
@@ -22,8 +22,9 @@ function createContractApplier<Raw, Data extends Raw, Error extends Raw>(
       }
 
       return response;
-    }
-  );
+    },
+    sid: 'ff.applyContractFx',
+  });
 
   return applyContractFx;
 }
