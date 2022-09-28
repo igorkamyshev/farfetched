e3a922ac4e3b49a798cf409efac2d57a188a5fb6
Igor Kamyşev <igor@kamyshev.me>
2022-09-28 19:15:24 +0700

    Fix `createJsonQuery` kind (#118)
    * Fix kind of Query created by createJsonQuery
    
    * docs(changeset): Fix `createJsonQuery` return object structure, now it passes `isQuery` check

---
+++ packages/core/src/query/__tests__/create_json_query.spec.ts
@@ -0,0 +1,28 @@
+import { createJsonQuery } from '../create_json_query';
+import { isQuery } from '../type';
+
+describe('createJsonQuery', () => {
+  // Does not matter
+  const response = {
+    contract: {
+      isData: (raw: unknown): raw is unknown => true,
+      getErrorMessages: () => [],
+    },
+    mapData: <T>(v: T) => v,
+  };
+
+  // Does not matter
+  const request = {
+    url: 'http://api.salo.com',
+    method: 'GET' as const,
+  };
+
+  test('isQuery', () => {
+    const query = createJsonQuery({
+      request,
+      response,
+    });
+
+    expect(isQuery(query)).toBeTruthy();
+  });
+});
