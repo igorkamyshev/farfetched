26b7302b787c714eeb132c07894b3f541c63dee4
Igor Kamyshev <igor@kamyshev.me>
2022-09-28 19:16:34 +0700

    Add test isMutation

--- packages/core/src/mutation/__tests__/create_json_mutation.spec.ts
+++ packages/core/src/mutation/__tests__/create_json_mutation.spec.ts
@@ -2,8 +2,18 @@ import { allSettled, fork } from 'effector';
 
 import { unknownContract } from '../../contract/unknown_contract';
 import { createJsonMutation } from '../create_json_mutation';
+import { isMutation } from '../type';
 
 describe('createJsonMutation', () => {
+  test('isMutation', () => {
+    const mutation = createJsonMutation({
+      request: { url: 'https://api.salo.com', method: 'GET' },
+      response: { contract: unknownContract },
+    });
+
+    expect(isMutation(mutation)).toBeTruthy();
+  });
+
   test('start triggers executeFx', async () => {
     const mutation = createJsonMutation({
       request: { url: 'https://api.salo.com', method: 'GET' },
