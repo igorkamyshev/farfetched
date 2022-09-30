f83d6720ba9a1cf3edb820396c2a1fdc7fec08c4
Igor Kamyshev <igor@kamyshev.me>
2022-09-30 19:58:29 +0700

    Add test for sampling real Query

--- packages/core/src/errors/__test__/guards.type_spec.ts
+++ packages/core/src/errors/__test__/guards.type_spec.ts
@@ -1,5 +1,15 @@
+import { sample } from 'effector';
+
 import { isHttpError } from '../guards';
+import { Query } from '../../query/type';
 
 const error: unknown = {};
 
 isHttpError({ error });
+
+const query: Query<void, unknown, unknown> = {} as any;
+
+sample({
+  clock: query.finished.failure,
+  filter: isHttpError,
+});
