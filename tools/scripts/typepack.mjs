a5bfcddee6b4e310934a3ef66cd6ae2ea81d2852
Igor Kamyshev <igor@kamyshev.me>
2022-11-24 12:59:23 +0700

    Finalize typepack script

--- tools/scripts/typepack.mjs
+++ tools/scripts/typepack.mjs
@@ -1,6 +1,7 @@
-import path, { join } from 'node:path';
+import path from 'node:path';
 import { parseArgs } from 'node:util';
 import { rmdir } from 'node:fs/promises';
+import { readCachedProjectGraph } from '@nrwl/devkit';
 import dts from 'rollup-plugin-dts';
 import { rollup } from 'rollup';
 
@@ -11,10 +12,15 @@ const inputDir = path.join('dist', 'packages', args.values.package);
 const inputFile = path.join(inputDir, 'index.d.ts');
 const outputFile = inputFile;
 
+const external = Object.values(readCachedProjectGraph().nodes)
+  .filter((node) => node.type === 'lib')
+  .map((node) => `@farfetched/${node.name}`);
+
 const bundle = await rollup({
   input: inputFile,
   plugins: [dts()],
+  external,
 });
 
 await bundle.write({ file: outputFile, format: 'es' });
-await rmdir(join(inputDir, 'src'), { recursive: true });
+await rmdir(path.join(inputDir, 'src'), { recursive: true });
