dcbdcaa042e8d446abfb920113d3b4ddf9076ca9
Igor Kamyshev <igor@kamyshev.me>
2022-12-26 19:11:10 +0700

    Support more formats in _Time_

--- packages/core/src/libs/date-nfs/time.ts
+++ packages/core/src/libs/date-nfs/time.ts
@@ -1,14 +1,41 @@
-type Min = `${number}min`;
-type Sec = `${number}sec`;
-type Hour = `${number}h`;
+type MillisecondUnit = 'ms' | 'milli' | 'millisecond' | 'milliseconds';
+type Millisecond = `${number}${MillisecondUnit}`;
+const millisecondUnits: MillisecondUnit[] = [
+  'ms',
+  'milli',
+  'millisecond',
+  'milliseconds',
+];
+
+type SecUnit = 's' | 'sec' | 'secs' | 'second' | 'seconds';
+type Sec = `${number}${SecUnit}`;
+const secUnits: SecUnit[] = ['s', 'sec', 'secs', 'second', 'seconds'];
+
+type MinUnit = 'm' | 'min' | 'mins' | 'minute' | 'minutes';
+type Min = `${number}${MinUnit}`;
+const minUnits: MinUnit[] = ['m', 'min', 'mins', 'minute', 'minutes'];
+
+type HourUnit = 'h' | 'hr' | 'hrs' | 'hour' | 'hours';
+type Hour = `${number}${HourUnit}`;
+const hourUnits: HourUnit[] = ['h', 'hr', 'hrs', 'hour', 'hours'];
 
 export type Time =
+  // Without milliseconds
   | `${Hour} ${Min} ${Sec}`
   | `${Hour} ${Min}`
   | `${Min} ${Sec}`
   | `${Hour}`
   | `${Min}`
   | `${Sec}`
+  // With milliseconds
+  | `${Hour} ${Min} ${Sec} ${Millisecond}`
+  | `${Hour} ${Min} ${Millisecond}`
+  | `${Min} ${Sec} ${Millisecond}`
+  | `${Hour} ${Millisecond}`
+  | `${Min} ${Millisecond}`
+  | `${Sec} ${Millisecond}`
+  | `${Millisecond}`
+  // Only milliseconds
   | number;
 
 export function parseTime(time: Time): number {
@@ -19,16 +46,33 @@ export function parseTime(time: Time): number {
   let result = 0;
 
   for (const part of time.split(' ')) {
-    if (part.endsWith('sec')) {
-      result += parseInt(part) * 1000;
-    }
-    if (part.endsWith('min')) {
-      result += parseInt(part) * 60000;
-    }
-    if (part.endsWith('h')) {
-      result += parseInt(part) * 3600000;
+    switch (true) {
+      case hasEnding(part, millisecondUnits):
+        result += parseNumber(part);
+        break;
+      case hasEnding(part, secUnits):
+        result += parseNumber(part) * 1000;
+        break;
+      case hasEnding(part, minUnits):
+        result += parseNumber(part) * 60000;
+        break;
+      case hasEnding(part, hourUnits):
+        result += parseNumber(part) * 3600000;
+        break;
     }
   }
 
   return result;
+}
+
+function hasEnding(value: string, allowedEndings: string[]): boolean {
+  return allowedEndings.includes(extractNonNumeric(value));
+}
+
+function extractNonNumeric(value: string): string {
+  return value.replace(/[0-9.]/g, '');
+}
+
+function parseNumber(value: string): number {
+  return value.includes('.') ? parseFloat(value) : parseInt(value);
 }
