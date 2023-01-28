35b2357ecfbe1c776fbb6aa69a2b9dad675d582d
Kirill Mironov <vetrokm@gmail.com>
2023-01-28 09:56:18 +0000

    chore: migrate apps to nx vite executors

--- apps/showcase/forest-real-world-breaking-bad/vite.config.ts
+++ apps/showcase/forest-real-world-breaking-bad/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  cacheDir:
+    '../../../node_modules/.vite/showcase-forest-real-world-breaking-bad',
   plugins: [tsconfigPaths()],
   build: { outDir: '../../../dist/apps/showcase/solid-real-world-rick-morty' },
 });
