7393846d0a6a8f48a6b60ccc0c03d5d207230fd4
Igor Kamyshev <igor@kamyshev.me>
2023-01-28 17:40:23 +0700

    Name release

--- apps/website/docs/.vitepress/config.js
+++ apps/website/docs/.vitepress/config.js
@@ -316,7 +316,7 @@ export default withMermaid(
             text: 'Releases',
             collapsible: false,
             items: [
-              { text: 'v0.7', link: '/releases/0-7' },
+              { text: 'v0.7 Nam Phu Chet Si', link: '/releases/0-7' },
               { text: 'v0.6 Huai Nam Dang', link: '/releases/0-6' },
               { text: 'v0.5 Chew Lan', link: '/releases/0-5' },
               { text: 'v0.4', link: '/releases/0-4' },
