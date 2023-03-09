0f9f2f4b707b07e882964c2ff0f2a5e0ae4477ff
Igor Kamyshev <igor@kamyshev.me>
2023-03-09 15:28:37 +0700

    Ajust @@trigger protocol to real use-cases

--- packages/core/src/trigger_api/trigger_protocol.ts
+++ packages/core/src/trigger_api/trigger_protocol.ts
@@ -3,6 +3,6 @@ import { Event } from 'effector';
 export type TriggerProtocol = {
   '@@trigger': () => {
     setup?: Event<void>;
-    fired: Event<any>;
+    fired: Event<unknown> | Event<void>;
   };
 };
