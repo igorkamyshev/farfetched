5b8b08c0f3ae4ede5513e27189fd3d0efea377fe
Igor Kamyshev <igor@kamyshev.me>
2023-03-15 11:28:40 +0700

    Add test case

---
+++ packages/core/src/__tests__/share_cache_between_attached.test.ts
@@ -0,0 +1,27 @@
+import { allSettled, fork } from 'effector';
+import { describe, test, expect, vi } from 'vitest';
+
+import { createQuery } from '../query/create_query';
+import { attachOperation } from '../attach/attach';
+import { cache } from '../cache/cache';
+import { withFactory } from '../libs/patronus';
+
+describe('shared cache between base Query and attached one', async () => {
+  test('attached query uses cache from base query', async () => {
+    const baseHandler = vi.fn().mockResolvedValue('base result');
+
+    const baseQuery = withFactory({
+      fn: () => createQuery({ handler: baseHandler }),
+      sid: '1',
+    });
+    cache(baseQuery, { staleAfter: '1s' });
+    const attachedQuery = attachOperation(baseQuery);
+
+    const scope = fork();
+
+    await allSettled(attachedQuery.refresh, { scope });
+    await allSettled(attachedQuery.refresh, { scope });
+
+    expect(baseHandler).toBeCalledTimes(1)
+  });
+});
