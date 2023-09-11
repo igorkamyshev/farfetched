b6156830b0fee88329de3cc160640d03f89ad956
Igor Kamyshev <igor@kamyshev.me>
2023-09-11 11:15:06 +0700

    Add one more test case

--- packages/core/src/query/__tests__/query.aborted.test.ts
+++ packages/core/src/query/__tests__/query.aborted.test.ts
@@ -4,10 +4,11 @@ import { describe, test, expect, vi } from 'vitest';
 
 import { unknownContract } from '../../contract/unknown_contract';
 import { fetchFx } from '../../fetch/fetch';
+import { declareParams } from '../../remote_operation/params';
 import { createJsonQuery } from '../create_json_query';
 
 describe('Query#aborted', () => {
-  test('should be fired of operation is aborted', async () => {
+  test('should be fired of operation is aborted after initial', async () => {
     const abortedListener = vi.fn();
     const failureListener = vi.fn();
 
@@ -16,7 +17,7 @@ describe('Query#aborted', () => {
     const query = createJsonQuery({
       request: {
         method: 'GET',
-        url: '/api/aborted',
+        url: 'https://api.example.com/api/aborted',
       },
       response: {
         contract: unknownContract,
@@ -38,6 +39,51 @@ describe('Query#aborted', () => {
 
     expect(abortedListener).toBeCalledTimes(1);
     expect(failureListener).not.toBeCalledTimes(1);
-    expect(scope.getState(query.$status)).toBe('initial');
+    expect(scope.getState(query.$status)).toBe('initial'); // previous status
+  });
+
+  test('should be fired of operation is aborted after initial', async () => {
+    const abortedListener = vi.fn();
+    const failureListener = vi.fn();
+
+    const signal = createEvent();
+
+    const query = createJsonQuery({
+      params: declareParams<{ id: number }>(),
+      request: {
+        method: 'GET',
+        url: ({ id }) => `https://api.example.com/api/aborted/${id}`,
+      },
+      response: {
+        contract: unknownContract,
+      },
+      concurrency: { abort: signal },
+    });
+
+    const scope = fork({
+      handlers: [
+        [
+          fetchFx,
+          vi.fn(async () => setTimeout(100).then(() => new Response())),
+        ],
+      ],
+    });
+
+    createWatch({ unit: query.aborted, fn: abortedListener, scope });
+    createWatch({ unit: query.finished.failure, fn: failureListener, scope });
+
+    await allSettled(query.refresh, { scope, params: { id: 1 } });
+
+    expect(scope.getState(query.$error)).toBe(null);
+    expect(scope.getState(query.$status)).toBe('done');
+
+    allSettled(query.refresh, { scope, params: { id: 2 } });
+    allSettled(signal, { scope });
+
+    await allSettled(scope);
+
+    expect(abortedListener).toBeCalledTimes(1);
+    expect(failureListener).not.toBeCalledTimes(1);
+    expect(scope.getState(query.$status)).toBe('done'); // previous status
   });
 });
