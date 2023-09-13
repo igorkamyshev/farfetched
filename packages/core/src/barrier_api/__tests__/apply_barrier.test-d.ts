8acb6421a584a5c1cb78ca32614886a93d592e18
Igor Kamyshev <igor@kamyshev.me>
2023-09-13 12:36:37 +0700

    Apply barrier typings

---
+++ packages/core/src/barrier_api/__tests__/apply_barrier.test-d.ts
@@ -0,0 +1,83 @@
+import { describe, expectTypeOf, test } from 'vitest';
+
+import { Query } from '../../query/type';
+import { Mutation } from '../../mutation/type';
+import { Barrier } from '../type';
+import { applyBarrier } from '../apply_barrier';
+import { createStore } from 'effector';
+
+describe('applyBarrier', () => {
+  const barrier: Barrier = {} as any;
+
+  test('allow query with no params', () => {
+    const query: Query<void, unknown, unknown, unknown> = {} as any;
+
+    applyBarrier(query, { barrier });
+  });
+
+  test('allow query with params', () => {
+    const query: Query<{ id: number }, unknown, unknown, unknown> = {} as any;
+
+    applyBarrier(query, { barrier });
+  });
+
+  test('allow mutation with no params', () => {
+    const mutation: Mutation<void, unknown, unknown> = {} as any;
+
+    applyBarrier(mutation, { barrier });
+  });
+
+  test('allow mutation with params', () => {
+    const mutation: Mutation<{ id: number }, unknown, unknown> = {} as any;
+
+    applyBarrier(mutation, { barrier });
+  });
+
+  test('forbid resumeParams for no params Query', () => {
+    const query: Query<void, unknown, unknown, unknown> = {} as any;
+
+    // @ts-expect-error resumeParams is not allowed for Query<void, ...>
+    applyBarrier(query, { barrier, resumeParams });
+  });
+
+  test('infer resumeParams for Query with params in callback', () => {
+    const query: Query<{ id: number }, unknown, unknown, unknown> = {} as any;
+
+    applyBarrier(query, {
+      barrier,
+      resumeParams: ({ params }) => {
+        expectTypeOf(params).toEqualTypeOf<{ id: number }>();
+
+        return params;
+      },
+    });
+  });
+
+  test('result type of resumeParams matches Query params type', () => {
+    const query: Query<{ id: number }, unknown, unknown, unknown> = {} as any;
+
+    // @ts-expect-error id in resumeParams return type has to be number
+    applyBarrier(query, {
+      barrier,
+      resumeParams: ({ params }) => {
+        return { id: '' };
+      },
+    });
+  });
+
+  test('infer type of source in resumeParams', () => {
+    const query: Query<{ id: number }, unknown, unknown, unknown> = {} as any;
+
+    applyBarrier(query, {
+      barrier,
+      resumeParams: {
+        source: createStore(1),
+        fn: ({ params }, source) => {
+          expectTypeOf(source).toEqualTypeOf<number>();
+          expectTypeOf(params).toEqualTypeOf<{ id: number }>();
+          return params;
+        },
+      },
+    });
+  });
+});
--- packages/core/src/barrier_api/apply_barrier.ts
+++ packages/core/src/barrier_api/apply_barrier.ts
@@ -1,3 +1,42 @@
-export function applyBarrier(): void {
+import { DynamicallySourcedField } from '../libs/patronus';
+import {
+  RemoteOperation,
+  RemoteOperationParams,
+} from '../remote_operation/type';
+import { Barrier } from './type';
+
+export function applyBarrier<
+  O extends RemoteOperation<void, any, any, any, any>
+>(operation: O, config: { barrier: Barrier }): void;
+
+export function applyBarrier<
+  O extends RemoteOperation<any, any, any, any, any>,
+  ResumeParamsSource = void
+>(
+  operation: O,
+  config: {
+    barrier: Barrier;
+    resumeParams?: DynamicallySourcedField<
+      { params: RemoteOperationParams<O> },
+      RemoteOperationParams<O>,
+      ResumeParamsSource
+    >;
+  }
+): void;
+
+export function applyBarrier<
+  O extends RemoteOperation<any, any, any, any, any>,
+  ResumeParamsSource = void
+>(
+  operation: O,
+  config: {
+    barrier: Barrier;
+    resumtParams?: DynamicallySourcedField<
+      { params: RemoteOperationParams<O> },
+      RemoteOperationParams<O>,
+      ResumeParamsSource
+    >;
+  }
+): void {
   throw new Error('not implemented');
 }
