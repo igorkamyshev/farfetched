7f9d5c87c3d7efaa17445f1184ad9628e4de3b2c
AlexandrHoroshih <xad71rus@gmail.com>
2023-09-13 23:48:32 +0700

    Add timeout aborter implementation

--- packages/core/src/timeout/timeout.ts
+++ packages/core/src/timeout/timeout.ts
@@ -1,17 +1,40 @@
+import { attach, sample } from 'effector';
 import {
-  delay,
-  normalizeSourced,
   normalizeStaticOrReactive,
-  type DynamicallySourcedField,
-  type SourcedField,
   type StaticOrReactive,
 } from '../libs/patronus';
 import { type RemoteOperation } from '../remote_operation/type';
 import { type Time, parseTime } from '../libs/date-nfs';
+import { CallObject } from '../remote_operation/with_call_object';
+import { timeoutError } from '../errors/create_error';
 
 export function timeout<Q extends RemoteOperation<any, any, any, any>>(
   operation: Q,
   config: { after: StaticOrReactive<Time> }
 ): void {
-  // ok
+  const timeoutAbortFx = attach({
+    source: normalizeStaticOrReactive(config.after).map(parseTime),
+    effect(timeoutMs, callObj: CallObject) {
+      return new Promise<void>((resolve) => {
+        // Setup call abort by timeout
+        const timeout = setTimeout(() => {
+          callObj.abort(timeoutError({ timeout: timeoutMs }));
+          resolve();
+        }, timeoutMs);
+
+        // Setup timeout cleanup if call is finished before timeout
+        const cleanup = () => {
+          clearTimeout(timeout);
+          resolve();
+        };
+
+        callObj.promise?.then(cleanup, cleanup);
+      });
+    },
+  });
+
+  sample({
+    clock: operation.__.lowLevelAPI.callObjectCreated,
+    target: timeoutAbortFx,
+  });
 }
