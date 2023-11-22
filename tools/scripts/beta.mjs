fc5c8efcf05d908f9ec661ca0910739d9994d82a
Igor Kamyshev <igor@kamyshev.me>
2023-11-22 20:50:00 +0700

    Replace names for packages

--- tools/scripts/beta.mjs
+++ tools/scripts/beta.mjs
@@ -1,4 +1,10 @@
+import {
+  readCachedProjectGraph,
+  writeJsonFile,
+  readJsonFile,
+} from '@nrwl/devkit';
 import { spawnSync } from 'child_process';
+import { join } from 'path';
 
 const branch = spawnSync('git', ['branch', '--show-current'], {
   stdio: 'pipe',
@@ -13,3 +19,26 @@ spawnSync('pnpm', ['changeset', 'pre', 'enter', branch], {
 spawnSync('pnpm', ['changeset', 'version'], {
   stdio: 'inherit',
 });
+
+for (const [_name, project] of Object.entries(readCachedProjectGraph().nodes)) {
+  if (project.type !== 'lib') {
+    continue;
+  }
+
+  const packageJsonPath = join(project.data.sourceRoot, 'package.json');
+
+  const originalPackageJson = readJsonFile(packageJsonPath);
+
+  writeJsonFile(packageJsonPath, {
+    ...originalPackageJson,
+    name: adjustPackageName(originalPackageJson.name),
+  });
+}
+
+spawnSync('pnpm', ['changeset', 'pre', 'exit'], {
+  stdio: 'inherit',
+});
+
+function adjustPackageName(name) {
+  return name.replace('@farfetched/', '@igorkamyshev/farfetched-');
+}
