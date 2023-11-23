24e531c68ceff9f8c9d780f2bc5d4dd9a20d4b1b
Igor Kamyshev <igor@kamyshev.me>
2023-11-23 14:26:50 +0700

    Fix renaming

--- tools/scripts/rename.mjs
+++ tools/scripts/rename.mjs
@@ -8,7 +8,10 @@ export async function renameForGitHub() {
     name: adjustPackageName(originalPackageJson.name),
     peerDependencies: mapValues(
       originalPackageJson.peerDependencies ?? {},
-      (version, name) => `npm:${adjustPackageName(name)}@${version},`
+      (version, name) =>
+        name.includes('@farfetched')
+          ? `npm:${adjustPackageName(name)}@${version}`
+          : version
     ),
   });
 }
