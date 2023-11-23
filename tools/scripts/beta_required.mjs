979b689a106bb770123366478701621cc9bbcae8
Igor Kamyshev <igor@kamyshev.me>
2023-11-23 12:33:56 +0700

    Next

--- tools/scripts/beta_required.mjs
+++ tools/scripts/beta_required.mjs
@@ -3,9 +3,18 @@ import { join } from 'path';
 
 const [, , commentBody] = process.argv;
 
+const LINE_START = 'Used changeset: ';
+
+const previousChangesets = JSON.parse(
+  commentBody
+    .split('\n')
+    .find((line) => line.includes(LINE_START))
+    ?.replace(LINE_START, '') ?? '[]'
+);
+
 const usedChangesets = readJsonFile(
   join(process.cwd(), '.changeset', 'pre.json')
 ).changesets;
 
-logger.log(`commentBody="${commentBody}"`);
+logger.log(`previousChangesets="${JSON.stringify(previousChangesets)}"`);
 logger.log(`usedChangesets="${JSON.stringify(usedChangesets)}"`);
