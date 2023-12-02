fcda823533b08b6c71b08ec8dff2c212f63d1959
Igor Kamyshev <igor@kamyshev.me>
2023-12-02 13:27:23 +0700

    Fix building

--- tools/scripts/typepack.mjs
+++ tools/scripts/typepack.mjs
@@ -7,13 +7,19 @@ import { createRequire } from 'node:module';
 
 const require = createRequire(import.meta.url);
 
-const { readCachedProjectGraph } = require('@nx/devkit');
+const {
+  readCachedProjectGraph,
+  readJsonFile,
+  writeJsonFile,
+} = require('@nx/devkit');
 
 const args = parseArgs({ options: { package: { type: 'string' } } });
 
+const TYPINGS_FILE_NAME = 'index.cjs.d.ts';
+
 const inputDir = path.join('dist', 'packages', args.values.package);
 
-const inputFile = path.join(inputDir, 'index.cjs.d.ts');
+const inputFile = path.join(inputDir, TYPINGS_FILE_NAME);
 const outputFile = inputFile;
 
 const external = Object.values(readCachedProjectGraph().nodes)
@@ -28,3 +34,27 @@ const bundle = await rollup({
 
 await bundle.write({ file: outputFile, format: 'es' });
 await rmdir(path.join(inputDir, 'src'), { recursive: true });
+
+const originalPackageJson = readJsonFile(path.join(inputDir, 'package.json'));
+
+writeJsonFile(path.join(inputDir, 'package.json'), {
+  ...originalPackageJson,
+  types: `./${TYPINGS_FILE_NAME}`,
+  exports: mapValues(originalPackageJson.exports ?? {}, (value) =>
+    typeof value === 'object'
+      ? {
+          ...value,
+          types: `./${TYPINGS_FILE_NAME}`,
+        }
+      : value
+  ),
+});
+
+export function mapValues(val, fn) {
+  const mappedEntries = Object.entries(val).map(([key, value]) => [
+    key,
+    fn(value, key),
+  ]);
+
+  return Object.fromEntries(mappedEntries);
+}
