1497be4afc9f3a3d33b1490260bc81844ebaf03d
Igor Kamyshev <igor@kamyshev.me>
2023-12-08 17:58:41 +0700

    Fix incorrect usage of Vue

--- packages/dev-tools/src/ui/Search.vue
+++ packages/dev-tools/src/ui/Search.vue
@@ -1,6 +1,17 @@
 <script setup>
-defineProps(['value']);
-defineEmits(['update:value']);
+import { computed } from 'vue';
+
+const props = defineProps(['modelValue']);
+const emit = defineEmits(['update:modelValue']);
+
+const value = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value) {
+    emit('update:modelValue', value);
+  },
+});
 </script>
 
 <template>
