c67d09c2e9e086851e97c5ab3c19b9eb2580e078
Igor Kamyshev <igor@kamyshev.me>
2023-12-11 17:58:20 +0700

    COLORS

--- tools/scripts/publint.mjs
+++ tools/scripts/publint.mjs
@@ -1,6 +1,7 @@
 import { spawnSync } from 'node:child_process';
 import path from 'node:path';
 import { readdir } from 'node:fs/promises';
+import { strip } from 'ansicolor';
 
 const packages = await readdir(path.join('dist', 'packages'));
 
@@ -11,6 +12,7 @@ for (const pkg of packages) {
   const result = spawnSync('publint', [inputDir])
     .output.toString()
     .split(',')
+    .map(strip)
     .join('');
 
   console.log(result);
