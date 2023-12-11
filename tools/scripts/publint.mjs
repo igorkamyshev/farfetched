c3a52d2673459444949b067ee8683df6d4b0a9bb
Igor Kamyşev <igor@kamyshev.me>
2023-12-11 18:05:51 +0700

    publint (#423)
    * Setup publint
    
    * COLORS
    
    * COLORS
    
    * Extra line
    
    * prettify

---
+++ tools/scripts/publint.mjs
@@ -0,0 +1,20 @@
+import { spawnSync } from 'node:child_process';
+import path from 'node:path';
+import { readdir } from 'node:fs/promises';
+import { strip } from 'ansicolor';
+
+const packages = await readdir(path.join('dist', 'packages'));
+
+for (const pkg of packages) {
+  const inputDir = path.join('dist', 'packages', pkg);
+
+  /* Save to variable to strip colors */
+  const result = spawnSync('publint', [inputDir])
+    .output.toString()
+    .split(',')
+    .map(strip)
+    .join('')
+    .replaceAll('\n', '\n\n');
+
+  console.log(result);
+}
