588ebfa2f7eb6c0b2e9127333d993784dcc024a0
Igor Kamyshev <igor@kamyshev.me>
2024-01-26 18:38:40 +0100

    Migrate all builds

--- packages/superstruct/vite.config.ts
+++ packages/superstruct/vite.config.ts
@@ -1,6 +1,26 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [
+    tsconfigPaths(),
+    dts({
+      entryRoot: 'src',
+      tsConfigFilePath: 'tsconfig.lib.json',
+      skipDiagnostics: true,
+      rollupTypes: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: '@farfetched/superstruct',
+      fileName: 'superstruct',
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['superstruct'],
+    },
+  },
 });
