6654821520a66bdf3cc6706400212998c6e525d5
Igor Kamyshev <igor@kamyshev.me>
2024-01-26 18:26:06 +0100

    MIgrate io-ts build

--- packages/io-ts/vite.config.ts
+++ packages/io-ts/vite.config.ts
@@ -1,6 +1,30 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [
+    tsconfigPaths(),
+    dts({
+      entryRoot: 'src',
+      tsConfigFilePath: 'tsconfig.lib.json',
+      skipDiagnostics: true,
+      rollupTypes: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: '@farfetched/io-ts',
+      fileName: 'io-ts',
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: [
+        'io-ts',
+        /* Because we import something from io-ts/SomePath */
+        /^io-ts\/(.+)/,
+      ],
+    },
+  },
 });
