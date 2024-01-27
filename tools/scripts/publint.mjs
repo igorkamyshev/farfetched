2212645bd9a9c898fcfaeb901e0479f3114e6cb2
Igor Kamyshev <igor@kamyshev.me>
2024-01-27 12:42:10 +0100

    Embed publint to CI

--- tools/scripts/publint.mjs
+++ tools/scripts/publint.mjs
@@ -1,23 +1,17 @@
-import { spawnSync } from 'node:child_process';
-import path from 'node:path';
-import { readdir, stat } from 'node:fs/promises';
-import { strip } from 'ansicolor';
+import { publint } from 'publint';
+import { formatMessage } from 'publint/utils';
+import fs from 'node:fs/promises';
 
-const packages = await readdir(path.join('packages'));
+const { messages } = await publint({
+  pkgDir: '.',
+  strict: true,
+});
 
-for (const pkg of packages) {
-  const inputDir = path.join('packages', pkg);
+if (messages.length) {
+  const pkg = await fs.readFile('package.json', 'utf8').then(JSON.parse);
 
-  const stats = await stat(path.join('packages', pkg));
-  if (!stats.isDirectory()) continue;
-
-  /* Save to variable to strip colors */
-  const result = spawnSync('publint', [inputDir])
-    .output.toString()
-    .split(',')
-    .map(strip)
-    .join('')
-    .replaceAll('\n', '\n\n');
-
-  console.log(result);
+  for (const message of messages) {
+    console.log(formatMessage(message, pkg));
+  }
+  process.exit(1);
 }
