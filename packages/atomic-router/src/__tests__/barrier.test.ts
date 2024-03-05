cee42f6bbfc489f27e430c9cb134ba441c7b8d46
Igor Kamyshev <igor@kamyshev.me>
2024-03-05 15:57:51 +0700

    Introduce `barrierChain` operator to connect Barrier API and atomic-router

---
+++ packages/atomic-router/src/__tests__/barrier.test.ts
@@ -0,0 +1,49 @@
+import { describe, test, expect } from 'vitest';
+import { chainRoute, createRoute } from 'atomic-router';
+import { createBarrier } from '@farfetched/core';
+import { allSettled, createStore, fork } from 'effector';
+
+import { barrierChain } from '../barrier';
+
+describe('barrierChain', () => {
+  test.concurrent(
+    'route opens immediately if barrier is not active',
+    async () => {
+      const $active = createStore(false);
+      const barrier = createBarrier({ active: $active });
+
+      const route = createRoute();
+      const chained = chainRoute({ route, ...barrierChain(barrier) });
+
+      const scope = fork();
+
+      await allSettled(route.open, { scope });
+
+      expect(scope.getState(chained.$isOpened)).toBe(true);
+    }
+  );
+
+  test.concurrent('route opens only after barrier is deactived', async () => {
+    const $active = createStore(false);
+
+    const barrier = createBarrier({ active: $active });
+
+    const route = createRoute();
+    const chained = chainRoute({ route, ...barrierChain(barrier) });
+
+    const scope = fork();
+
+    allSettled($active, { scope, params: true });
+
+    // Do not await there, because allSettled will wait for the barrier to be deactivated
+    allSettled(route.open, { scope });
+
+    expect(scope.getState(route.$isOpened)).toBe(true);
+    expect(scope.getState(chained.$isOpened)).toBe(false);
+
+    await allSettled($active, { scope, params: false });
+
+    expect(scope.getState(route.$isOpened)).toBe(true);
+    expect(scope.getState(chained.$isOpened)).toBe(true);
+  });
+});
---
+++ packages/atomic-router/src/barrier.ts
@@ -0,0 +1,39 @@
+import { type Barrier } from '@farfetched/core';
+import { type RouteParamsAndQuery } from 'atomic-router';
+import { attach, createEvent, sample, scopeBind } from 'effector';
+
+import { type ChainProtocol } from './chain.ptorocol';
+
+export function barrierChain(barrier: Barrier): ChainProtocol<any> {
+  const beforeOpen = createEvent<RouteParamsAndQuery<any>>();
+
+  const openOn = createEvent();
+
+  /** Never called */
+  const cancelOn = createEvent();
+
+  const fx = attach({
+    source: barrier.__.$mutex,
+    async effect(mutex) {
+      const boundOpenOn = scopeBind(openOn, { safe: true });
+
+      await mutex?.waitForUnlock();
+
+      boundOpenOn();
+    },
+  });
+
+  sample({
+    clock: beforeOpen,
+    target: barrier.__.touch,
+    /* Use batch:false to increase priority of touch call */
+    batch: false,
+  });
+  sample({ clock: beforeOpen, target: fx });
+
+  return {
+    beforeOpen,
+    openOn,
+    cancelOn,
+  };
+}
