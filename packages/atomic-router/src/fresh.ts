35daf008e32c5a0d83f532547478a99828c21ef6
Rinat <r.ibrgimov@pnlfin.tech>
2024-04-05 11:16:48 +0300

    #464 mapParams into atomic-router chainRoute integration

--- packages/atomic-router/src/fresh.ts
+++ packages/atomic-router/src/fresh.ts
@@ -5,15 +5,18 @@ import { createEvent, sample } from 'effector';
 import { type ChainProtocol } from './chain.ptorocol';
 
 export function freshChain(
-  query: Query<void, any, any, any>
+  query: Query<void, any, any, any>,
+  mapParams?: ({ params }: { params: any }) => void
 ): ChainProtocol<any>;
 
-export function freshChain<RouteParams extends Record<string, any>>(
-  query: Query<RouteParams, any, any, any>
+export function freshChain<RouteParams extends Record<string, any>, QueryParams extends any>(
+  query: Query<QueryParams, any, any, any>,
+  mapParams?: ({ params }: { params: RouteParams }) => QueryParams
 ): ChainProtocol<RouteParams>;
 
 export function freshChain(
-  query: Query<any, any, any, any>
+  query: Query<any, any, any, any>,
+  mapParams: ({ params }: { params: any }) => any = ({ params }) => params
 ): ChainProtocol<any> {
   const beforeOpen = createEvent<RouteParamsAndQuery<any>>();
   const openOn = createEvent<any>();
@@ -21,7 +24,7 @@ export function freshChain(
 
   sample({
     clock: beforeOpen,
-    fn: ({ params }) => params,
+    fn: mapParams,
     target: query.refresh,
   });
   sample({
--- packages/atomic-router/src/start.ts
+++ packages/atomic-router/src/start.ts
@@ -5,15 +5,18 @@ import { createEvent, sample } from 'effector';
 import { type ChainProtocol } from './chain.ptorocol';
 
 export function startChain(
-  query: Query<void, any, any, any>
+  query: Query<void, any, any, any>,
+  mapParams?: ({ params }: { params: any }) => void
 ): ChainProtocol<any>;
 
-export function startChain<RouteParams extends Record<string, any>>(
-  query: Query<RouteParams, any, any, any>
+export function startChain<RouteParams extends Record<string, any>, QueryParams extends any>(
+  query: Query<RouteParams, any, any, any>,
+  mapParams?: ({ params }: { params: RouteParams }) => QueryParams
 ): ChainProtocol<RouteParams>;
 
 export function startChain(
-  query: Query<any, any, any, any>
+  query: Query<any, any, any, any>,
+  mapParams: ({ params }: { params: any }) => any = ({ params }) => params
 ): ChainProtocol<any> {
   const beforeOpen = createEvent<RouteParamsAndQuery<any>>();
   const openOn = createEvent<any>();
@@ -21,7 +24,7 @@ export function startChain(
 
   sample({
     clock: beforeOpen,
-    fn: ({ params }) => params,
+    fn: mapParams,
     target: query.start,
   });
   sample({ clock: query.finished.success, target: openOn });
