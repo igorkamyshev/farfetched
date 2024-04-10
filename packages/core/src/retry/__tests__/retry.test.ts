35bbe3684c05625a93b4c942503acc3bebb075b2
Igor Kamyshev <igor@kamyshev.me>
2024-04-10 11:51:26 +0700

    Add test case

--- packages/core/src/retry/__tests__/retry.test.ts
+++ packages/core/src/retry/__tests__/retry.test.ts
@@ -4,6 +4,8 @@ import { allSettled, createWatch, fork } from 'effector';
 import { unknownContract } from '../../contract/unknown_contract';
 import { createJsonMutation } from '../../mutation/create_json_mutation';
 import { httpError } from '../../errors/create_error';
+import { createJsonQuery } from '../../query/create_json_query';
+import { fetchFx } from '../../fetch/fetch';
 import { retry } from '../retry';
 
 describe('retry', () => {
@@ -47,5 +49,38 @@ describe('retry', () => {
     await allSettled(m.start, { scope });
 
     expect(onFailure).toBeCalledTimes(1);
+  });
+
+  test.only('does supress contract failure error as well, issue #459', async () => {
+    const q = createJsonQuery({
+      request: { url: 'https://api.salo.com', method: 'GET' },
+      response: {
+        contract: {
+          isData(v): v is unknown {
+            return false;
+          },
+          getErrorMessages(v) {
+            return ['Failed contract'];
+          },
+        },
+      },
+    });
+
+    retry(q, { times: 1, delay: 1 });
+
+    const scope = fork({
+      handlers: [[fetchFx, () => new Response('{"data": "ok"}')]],
+    });
+
+    const failedListener = vi.fn();
+    createWatch({ unit: q.finished.failure, scope, fn: failedListener });
+
+    const startedListener = vi.fn();
+    createWatch({ unit: q.started, scope, fn: startedListener });
+
+    await allSettled(q.start, { scope });
+
+    expect(startedListener).toBeCalledTimes(2 /* Initial and retry */);
+    expect(failedListener).toBeCalledTimes(1 /* Only latest failure */);
   });
 });
