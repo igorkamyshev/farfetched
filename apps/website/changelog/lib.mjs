748df706914e934273837f35fdea2509ed471b72
Igor Kamyshev <igor@kamyshev.me>
2024-08-17 16:19:02 +0700

    Make changelog on website more clear

--- apps/website/changelog/lib.mjs
+++ apps/website/changelog/lib.mjs
@@ -26,9 +26,9 @@ export function excludeTrashUpdates(items) {
     .map((tags) => {
       const [header, ...body] = tags;
 
-      const filteredBody = body.filter(
-        (item) => !item.at(1).toLowerCase().includes('updated dependencies')
-      );
+      const filteredBody = body
+        .filter((item) => !isUpdatedDependenciesMessage(item))
+        .filter((item) => !isPackageVersionBumpMessage(item));
 
       if (filteredBody.length === 0) {
         return null;
@@ -37,4 +37,36 @@ export function excludeTrashUpdates(items) {
       return [header, ...filteredBody];
     })
     .filter(Boolean);
+}
+
+function isUpdatedDependenciesMessage(item) {
+  return item.at(1).toLowerCase().includes('updated dependencies');
+}
+
+function isPackageVersionBumpMessage(item) {
+  if (!Array.isArray(item)) {
+    return false;
+  }
+
+  if (item.at(0) === 'bulletlist') {
+    return item.some(isPackageVersionBumpMessage);
+  }
+
+  if (item.at(0) !== 'listitem') {
+    return false;
+  }
+
+  if (item.length === 2) {
+    const message = item.at(1);
+    const [org, pkgAndVersion] = message.split('/');
+    const [pkg, version] = pkgAndVersion?.split('@') ?? [];
+
+    if (!org || !pkg || !version) {
+      return false;
+    }
+
+    return org === '@farfetched';
+  } else {
+    return item.some(isPackageVersionBumpMessage);
+  }
 }
