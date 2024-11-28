b92e16bbd3eec306db9a71c5baec289e350c76d1
Igor Kamyshev <igor@kamyshev.me>
2024-11-28 12:59:23 +0700

    Format

--- packages/atomic-router/src/__tests__/barrier.test.ts
+++ packages/atomic-router/src/__tests__/barrier.test.ts
@@ -7,22 +7,19 @@ import { barrierChain } from '../barrier';
 
 describe('barrierChain', () => {
   // TODO: enable back and debug why it fails
-  test.skip(
-    'route opens immediately if barrier is not active',
-    async () => {
-      const $active = createStore(false);
-      const barrier = createBarrier({ active: $active });
+  test.skip('route opens immediately if barrier is not active', async () => {
+    const $active = createStore(false);
+    const barrier = createBarrier({ active: $active });
 
-      const route = createRoute();
-      const chained = chainRoute({ route, ...barrierChain(barrier) });
+    const route = createRoute();
+    const chained = chainRoute({ route, ...barrierChain(barrier) });
 
-      const scope = fork();
+    const scope = fork();
 
-      await allSettled(route.open, { scope });
+    await allSettled(route.open, { scope });
 
-      expect(scope.getState(chained.$isOpened)).toBe(true);
-    }
-  );
+    expect(scope.getState(chained.$isOpened)).toBe(true);
+  });
 
   test.concurrent('route opens only after barrier is deactived', async () => {
     const $active = createStore(false);
